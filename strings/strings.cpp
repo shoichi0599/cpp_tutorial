e746af6743eca208d200034506b3e0287b4587be
shoichisato <shoichi.programming@gmail.com>
2018-11-17 13:39:54 +0900

    finish string

---
+++ strings/strings.cpp
@@ -0,0 +1,16 @@
+// Name : strings.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string text1 = "Hello ";
+    string text2 = "Fred";
+    string text3 = text1 + text2;
+
+    cout << text1 << text2 << endl;
+    cout << text3 << endl;
+
+    return 0;
+}
