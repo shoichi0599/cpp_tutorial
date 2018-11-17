25880207829840db58f13dde43785c2dfc112e26
shoichisato <shoichi.programming@gmail.com>
2018-11-17 13:12:14 +0900

    finish hello world, outputting file, variables

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,11 @@
+// Hellow World
+// A basic program
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ outputtiing_file/outputting_file.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Starting program..." << flush << endl;
+
+    cout << "Banna." << "Apple." << "Orange." << endl;
+
+    cout << "This is same text." << endl;
+
+    cout << "This is some more text" << endl;
+
+    return 0;
+}
