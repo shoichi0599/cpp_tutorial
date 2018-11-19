0924f023bf07c52347989eb2a6c79bffc2f85021
shoichisato <shoichi.programming@gmail.com>
2018-11-19 14:19:09 +0900

    finish size of arrays

---
+++ sizeof/sizeof.cpp
@@ -0,0 +1,20 @@
+// Name : sizeof.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int values[] = {4, 7, 3, 4};
+
+    cout << "sizeof(values): " << sizeof(values) << endl;
+    cout << "sizeof(int)   : " << sizeof(int) << endl;
+
+    for (int i = 0; i < sizeof(values)/sizeof(int); i++) {
+        cout << values[i] << " " << flush;
+    }
+
+    cout << endl;
+
+    return 0;
+}
